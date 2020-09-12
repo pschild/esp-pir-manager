105213ea5877c95f9a5dc788de45d14432a58d75
pschild <philippe.schild@gmail.com>
2020-09-12 18:19:15 +0200

    moved env file

--- alexa-remote-control/alexa_remote_control.sh
+++ alexa-remote-control/alexa_remote_control.sh
@@ -68,8 +68,8 @@
 #
 ##########################################
 
-SET_EMAIL=$(grep AMAZON_EMAIL ../alexa-remote-control-variables.env | cut -d '=' -f2)
-SET_PASSWORD=$(grep AMAZON_PASSWORD ../alexa-remote-control-variables.env | cut -d '=' -f2)
+SET_EMAIL=$(grep AMAZON_EMAIL variables.env | cut -d '=' -f2)
+SET_PASSWORD=$(grep AMAZON_PASSWORD variables.env | cut -d '=' -f2)
 SET_MFA_SECRET=''
 # something like:
 #  1234 5678 9ABC DEFG HIJK LMNO PQRS TUVW XYZ0 1234 5678 9ABC DEFG
