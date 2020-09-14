309bc42ba7bb17a396d7fd34bab222e1a870f369
Philippe Schild <pschild@users.noreply.github.com>
2020-09-14 10:44:06 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,11 @@ import * as express from 'express';
 import { Application, Request, Response } from 'express';
 import * as fs from 'fs';
 import axios from 'axios';
-var exec = require('child_process').exec;
+// var exec = require('child_process').exec;
+const { exec } = require('child_process');
 
 const app: Application = express();
 const port = 9052;
-
-/*const LIMIT = 60 * 60 * 1000; // 60 minutes
-let cachedDate;*/
 
 app.get('/', (req, res) => {
     console.log(JSON.stringify(req.headers));
@@ -16,54 +14,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/on', (req, res) => {
-    //exec("./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e automation:'Kleines Licht'", console.log);
-    exec("./alexa-remote-control/alexa_remote_control.sh -a", console.log);
-    res.status(200).json({ status: 'ready' });
-});
-
-/*app.get('/on', (req, res) => {
-    axios.get('http://192.168.178.48/on').then(r => {
-        res.status(200).json({ status: 'on' });
-    });
-});
-
-app.get('/off', (req, res) => {
-    axios.get('http://192.168.178.48/off').then(r => {
-        res.status(200).json({ status: 'off' });
-    });
-});
-
-const onForTimespan = (seconds: number) => {
-    axios.get('http://192.168.178.48/on').then(r => {
-        setTimeout(() => { axios.get('http://192.168.178.48/off'); }, seconds * 1000);
-    });
-};
-
-app.post('/movement', (req: Request, res: Response) => {
-    const now = new Date();
-    let logMessage = `${now} movement detected!`;
-    
-    // onForTimespan(3 * 60);
-
-    if (!cachedDate || cachedDate.getTime() + LIMIT < now.getTime()) {
-        // exec("./alexa_remote_control.sh -d 'Philippes Echo Flex' -e automation:'Kleines Licht'", puts);
-        console.log(`${now.toISOString()}: triggered!`);
-        logMessage += ` triggered!\n`;
-        cachedDate = now;
-    } else {
-        console.log(`${now.toISOString()}: limit reached, skipping...`);
-        logMessage += ` skipping!\n`;
-    }
-
-    fs.appendFile('movements.log', logMessage, (err) => {
-        if (err) {
-            return res.status(500).send(`Error writing to file!`);
+    // exec(`./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e automation:'Kleines Licht'`, (error, stdout, stderr) => {
+    // exec(`./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e speak:'Das ist ein Test'`, (error, stdout, stderr) => {
+    exec(`./alexa-remote-control/alexa_remote_control.sh -a`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).json({ status: 'error', error });
         }
-        // console.log(`Saved @ ${now.toISOString()}!`);
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        return res.status(200).json({ status: 'success', stdout });
     });
-
-    res.status(200).end();
-});*/
+});
 
 app.listen(port, () => {
     console.log(`running at http://localhost:${port}`);
