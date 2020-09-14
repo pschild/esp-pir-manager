044bdb01ecff04307d65d1c7d52ea688c274dacf
pschild <philippe.schild@gmail.com>
2020-09-14 09:20:12 +0200

    fixed path

--- alexa-remote-control/alexa_remote_control.sh
+++ alexa-remote-control/alexa_remote_control.sh
@@ -69,11 +69,11 @@
 ##########################################
 
 DIRECTORY=$(cd `dirname $0` && pwd)
-SET_EMAIL=$(grep AMAZON_EMAIL ${DIRECTORY}/variables.env | cut -d '=' -f2)
+SET_EMAIL=$(grep AMAZON_EMAIL ${DIRECTORY}/env/variables.env | cut -d '=' -f2)
 echo $SET_EMAIL
-SET_PASSWORD=$(grep AMAZON_PASSWORD ${DIRECTORY}/variables.env | cut -d '=' -f2)
+SET_PASSWORD=$(grep AMAZON_PASSWORD ${DIRECTORY}/env/variables.env | cut -d '=' -f2)
 echo $SET_PASSWORD
-SET_MFA_SECRET=$(grep AMAZON_MFA_SECRET ${DIRECTORY}/variables.env | cut -d '=' -f2)
+SET_MFA_SECRET=$(grep AMAZON_MFA_SECRET ${DIRECTORY}/env/variables.env | cut -d '=' -f2)
 echo $SET_MFA_SECRET
 # something like:
 #  1234 5678 9ABC DEFG HIJK LMNO PQRS TUVW XYZ0 1234 5678 9ABC DEFG
