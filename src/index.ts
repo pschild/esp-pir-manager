d5135ac1ba69c3af75cbaec5369caa10cd0a7851
Philippe Schild <pschild@users.noreply.github.com>
2020-09-14 13:10:47 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,40 @@ import * as express from 'express';
 import { Application, Request, Response } from 'express';
 import * as fs from 'fs';
 import axios from 'axios';
-// var exec = require('child_process').exec;
 const { exec } = require('child_process');
 
 const app: Application = express();
 const port = 9052;
 
-app.get('/', (req, res) => {
-    console.log(JSON.stringify(req.headers));
-    res.status(200).json({ status: 'ready' });
+app.get('/speak/:speech', (req, res) => {
+    const speech = req.params.speech;
+    console.log(speech);
+    exec(`./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e speak:'${speech}'`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).json({ status: 'error', error });
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        return res.status(200).json({ status: 'success', stdout });
+    });
 });
 
-app.get('/on', (req, res) => {
-    // exec(`./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e automation:'Kleines Licht'`, (error, stdout, stderr) => {
-    // exec(`./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e speak:'Das ist ein Test'`, (error, stdout, stderr) => {
+app.get('/automation/:routineName', (req, res) => {
+    const routineName = req.params.routineName; // e.g. Kleines Licht
+    console.log(routineName);
+    exec(`./alexa-remote-control/alexa_remote_control.sh -d 'Philippes Echo Flex' -e automation:'${routineName}'`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return res.status(500).json({ status: 'error', error });
+        }
+        console.log(`stdout: ${stdout}`);
+        console.error(`stderr: ${stderr}`);
+        return res.status(200).json({ status: 'success', stdout });
+    });
+});
+
+app.get('/show-alexa-devices', (req, res) => {
     exec(`./alexa-remote-control/alexa_remote_control.sh -a`, (error, stdout, stderr) => {
         if (error) {
             console.error(`exec error: ${error}`);
